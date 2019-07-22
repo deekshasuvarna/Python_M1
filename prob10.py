c66c3e4d584750d81aaf822bc0a5d6e62c371ed0
dikshasuvarna <dikshasuvarna855@gmail.com>
2019-07-22 16:15:12 +0530

    Implementation

---
+++ prob10.py
@@ -0,0 +1,8 @@
+"""10.	Write a program to print the following output pattern.
+         1
+        121
+       12321
+      1234321
+     123454321"""
+
+
---
+++ prob8.py
@@ -0,0 +1,7 @@
+"""8.	Write a program to accept 2 numbers “m” and “n” from the user;
+ determine the value of m^n without using predefined functions"""
+x=0
+m=int(input("enter the value of m"))
+n=int(input("enter the value of n"))
+x=m**n
+print(f"The result:m^n={x}")
